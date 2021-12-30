4281bfe11f3684dab11b6c00c19243cc0e6cd419
Frédérique Mittelstaedt <git@gfm.io>
2021-12-30 23:53:17 +0000

    add home.nix

---
+++ home.nix
@@ -0,0 +1,78 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "gfmio";
+  home.homeDirectory = "/home/gfmio";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  #
+  # X session
+  #
+
+  xsession.enable = true;
+  xsession.windowManager.command = "i3";
+
+  #
+  # VS Code
+  #
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      # Some example extensions...
+      dracula-theme.theme-dracula
+      vscodevim.vim
+      yzhang.markdown-all-in-one
+    ];
+  };
+
+  #
+  # Sublime Text
+  #
+
+  home.packages = with pkgs; [ sublime4 ];
+
+  #
+  # git
+  #
+
+  programs.git = {
+    enable = true;
+    userName = "Frédérique Mittelstaedt";
+    userEmail = "git@gfm.io";
+  };
+
+  #
+  # ssh
+  #
+
+  programs.ssh = {
+    enable = true;
+  };
+
+  #
+  # htop
+  #
+
+  programs.htop = {
+    enable = true;
+    # Detailed CPU time (System/IO-Wait/Hard-IRQ/Soft-IRQ/Steal/Guest).
+    detailedCpuTime = true;
+  };
+}
