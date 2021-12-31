cfa6134fcb81f1b500d939001aa9808465be733f
Frédérique Mittelstaedt <git@gfm.io>
2021-12-31 03:18:18 +0000

    add creation of home config file

--- install-inside.sh
+++ install-inside.sh
@@ -4,3 +4,6 @@
 
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
+
+mkdir -p $HOME/.config/nixpkgs
+touch $HOME/.config/nixpkgs/home.nix
