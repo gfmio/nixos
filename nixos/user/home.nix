a98d60538c02a13e16589ed0da331e04aefc5101
Frédérique Mittelstaedt <git@gfm.io>
2022-01-17 21:20:40 +0000

    add vscode extensions to home.nix

--- nixos/user/home.nix
+++ nixos/user/home.nix
@@ -45,18 +45,115 @@
       dbaeumer.vscode-eslint
       editorconfig.editorconfig
       esbenp.prettier-vscode
-      github.codespaces
+      # github.codespaces
       ms-azuretools.vscode-docker
       ms-python.python
       ms-python.vscode-pylance
       ms-toolsai.jupyter
-      ms-toolsai.jupyter-keymap
-      ms-toolsai.jupyter-renderers
-      ms-vscode-remote.remote-containers
-      ms-vscode.hexeditor
-      ms-vscode.sublime-keybindings
+      # "ms-toolsai.jupyter-keymap"
+      # "ms-toolsai.jupyter-renderers"
+      # "ms-vscode-remote.remote-containers"
+      # "ms-vscode.hexeditor"
+      # "ms-vscode.sublime-keybindings"
       vscodevim.vim
-      wholroyd.hcl
+      # "wholroyd.hcl"
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "Nix";
+        publisher = "bbenoist";
+        version = "1.0.1";
+        sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
+      }
+      {
+        name = "vscode-eslint";
+        publisher = "dbaeumer";
+        version = "2.2.3";
+        sha256 = "0sl9d85wbac3h79a5y5mcv0rhcz8azcamyiiyix0b7klcr80v56d";
+      }
+      {
+        name = "EditorConfig";
+        publisher = "EditorConfig";
+        version = "0.16.4";
+        sha256 = "0fa4h9hk1xq6j3zfxvf483sbb4bd17fjl5cdm3rll7z9kaigdqwg";
+      }
+      {
+        name = "prettier-vscode";
+        publisher = "esbenp";
+        version = "9.1.0";
+        sha256 = "1vfyzxaqj0bdl9cd0x6glnydbi85pf4j9fbasj2i0lfqn06l27lm";
+      }
+      {
+        name = "codespaces";
+        publisher = "GitHub";
+        version = "1.4.4";
+        sha256 = "0jq7gnparjsvjbxxylvhmjq1myw1kipk3d25m31q005k242rgy54";
+      }
+      {
+        name = "vscode-docker";
+        publisher = "ms-azuretools";
+        version = "1.18.0";
+        sha256 = "0hhlhx3xy7x31xx2v3srvk67immajs6dm9h0wi49ii1rwx61zxah";
+      }
+      {
+        name = "python";
+        publisher = "ms-python";
+        version = "2021.12.1559732655";
+        sha256 = "0ghwj1n57zgfqnlwdxy18ahkljixv6dd2810rzw6vfqvp1kxax45";
+      }
+      {
+        name = "vscode-pylance";
+        publisher = "ms-python";
+        version = "2022.1.1";
+        sha256 = "0v5ynwbjyh3ggw2m0j2dpx1yx691zi3w3rmlm97ngjy6p3ldr701";
+      }
+      {
+        name = "jupyter";
+        publisher = "ms-toolsai";
+        version = "2022.1.1001707094";
+        sha256 = "0gb9znvakcqbimvnnl4qkr6lyyvw0zvaya658zqfxfcf3fyfs59x";
+      }
+      {
+        name = "jupyter-keymap";
+        publisher = "ms-toolsai";
+        version = "1.0.0";
+        sha256 = "0wkwllghadil9hk6zamh9brhgn539yhz6dlr97bzf9szyd36dzv8";
+      }
+      {
+        name = "jupyter-renderers";
+        publisher = "ms-toolsai";
+        version = "1.0.4";
+        sha256 = "01cspdjh40kzf015vqydyhcxlh7iqrgr16px181nyzvldb8ax9b8";
+      }
+      {
+        name = "remote-containers";
+        publisher = "ms-vscode-remote";
+        version = "0.212.0";
+        sha256 = "0b6bycjbkx1rgbyi48kszyngzm85ii03am9l83kp2kba113x1fy3";
+      }
+      {
+        name = "hexeditor";
+        publisher = "ms-vscode";
+        version = "1.9.3";
+        sha256 = "03093mx22ckcf0nrj1ik9ciqb2z6y8gidjny60a8lwqniihxw8qc";
+      }
+      {
+        name = "sublime-keybindings";
+        publisher = "ms-vscode";
+        version = "4.0.10";
+        sha256 = "0l8z0sv3432qrzh6118km7xr7g93fajmjihw8md47kfsdl9c4xxg";
+      }
+      {
+        name = "vim";
+        publisher = "vscodevim";
+        version = "1.21.10";
+        sha256 = "0c9m7mc2kmfzj3hkwq3d4hj43qha8a75q5r1rdf1xfx8wi5hhb1n";
+      }
+      {
+        name = "HCL";
+        publisher = "wholroyd";
+        version = "0.0.5";
+        sha256 = "12lx9gjizy4m6lqjv9c10kmncdbbw7mrq69a5qisr69g9w9h6hwp";
+      }
     ];
   };
 
