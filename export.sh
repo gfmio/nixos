5d68757ad0d122fcc3c826991509f5cd93b3f9b8
Frédérique Mittelstaedt <git@gfm.io>
2022-01-17 23:02:44 +0000

    add config.nix

--- export.sh
+++ export.sh
@@ -3,4 +3,5 @@
 cat nixos/system/configuration.nix | sudo tee /etc/nixos/configuration.nix
 # cat nixos/system/hardware-configuration.nix | sudo tee /etc/nixos/hardware-configuration.nix
 
-cat nixos/user/home.nix | sudo tee $HOME/.config/nixpkgs/home.nix
+cat nixos/user/home.nix | tee $HOME/.config/nixpkgs/home.nix
+cat nixos/user/config.nix | tee $HOME/.config/nixpkgs/config.nix
--- import.sh
+++ import.sh
@@ -4,3 +4,4 @@ cat /etc/nixos/configuration.nix > nixos/system/configuration.nix
 cat /etc/nixos/hardware-configuration.nix > nixos/system/hardware-configuration.nix
 
 cat $HOME/.config/nixpkgs/home.nix > nixos/user/home.nix
+cat $HOME/.config/nixpkgs/config.nix > nixos/user/config.nix
