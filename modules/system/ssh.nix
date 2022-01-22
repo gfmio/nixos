9fcd323895cbc2c0b47b174b1c300787afcd087d
Frédérique Mittelstaedt <git@gfm.io>
2022-01-22 21:00:11 +0000

    fix ssh config

--- modules/system/ssh.nix
+++ modules/system/ssh.nix
@@ -23,6 +23,6 @@ in
 
   config = mkIf cfg.enable {
     services.openssh.enable = true;
-    services.openssh.permitRootLogin = cfg.permitRootLogin;
+    services.openssh.permitRootLogin = if cfg.permitRootLogin then "yes" else "no";
   };
 }
