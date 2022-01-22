cfa5358a9094584eebb27363b867a9500d205768
Frédérique Mittelstaedt <git@gfm.io>
2022-01-22 12:18:37 +0000

    update export.sh

--- export.sh
+++ export.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-cat nixos/system/configuration.nix | sudo tee /etc/nixos/configuration.nix
-# cat nixos/system/hardware-configuration.nix | sudo tee /etc/nixos/hardware-configuration.nix
+# System
 
-cat nixos/user/home.nix | tee $HOME/.config/nixpkgs/home.nix
-cat nixos/user/config.nix | tee $HOME/.config/nixpkgs/config.nix
+cat hosts/nixos-test-vm/system/configuration.nix | sudo tee /etc/nixos/configuration.nix
+cat hosts/nixos-test-vm/system/common.nix | sudo tee /etc/nixos/common.nix
+# cat hosts/nixos-test-vm/system/hardware-configuration.nix | sudo tee /etc/nixos/hardware-configuration.nix
+sudo rm -rf /etc/nixos/modules
+sudo cp -R modules/system /etc/nixos/modules
+
+# User
+
+cat hosts/nixos-test-vm/user/home.nix | tee $HOME/.config/nixpkgs/home.nix
+cat hosts/nixos-test-vm/user/config.nix | tee $HOME/.config/nixpkgs/config.nix
+cat hosts/nixos-test-vm/user/common.nix | tee $HOME/.config/nixpkgs/common.nix
+rm -rf $HOME/.config/nixpkgs/modules
+cp -R modules/user $HOME/.config/nixpkgs/modules
