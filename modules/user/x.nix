0c53645567d957b74ab4e7837668163c8b42c954
Frédérique Mittelstaedt <git@gfm.io>
2022-01-22 14:42:44 +0000

    add x module

---
+++ modules/user/x.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.modules.x;
+in
+{
+  imports = [];
+
+  options = {
+    modules = {
+      x = {
+        enable = mkOption { type = types.bool; default = false; };
+        scale = mkOption { type = types.float; default = 1; };
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    xresources.properties = {
+      "Xft.dpi" = 96 * cfg.scale;
+      "Xft.autohint" = 0;
+      "Xft.lcdfilter" = "lcddefault";
+      "Xft.hintstyle" = "hintfull";
+      "Xft.hinting" = 1;
+      "Xft.antialias" = 1;
+      "Xft.rgba" = "rgb";
+    };
+
+    xsession.enable = true;
+  };
+}
