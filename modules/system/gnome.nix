6b274ec7b49baccde9f31a033875c2f8326b40af
Frédérique Mittelstaedt <git@gfm.io>
2022-01-23 23:47:51 +0000

    update gnome settings

--- modules/system/gnome.nix
+++ modules/system/gnome.nix
@@ -22,5 +22,19 @@ in
 
   config = mkIf cfg.enable {
     services.xserver.desktopManager.gnome.enable = true;
+
+    environment.gnome3.excludePackages = with pkgs.gnome3; [
+      epiphany
+      evolution
+      gnome-maps
+      accerciser
+    ];
+
+    # Enable Chrome GNOME Shell native host connector
+    # This is a DBus service allowing to install GNOME Shell extensions from a web browser.
+    services.gnome3.chrome-gnome-shell.enable = true;
+
+    # Enable GNOME Keyring daemon
+    services.gnome3.gnome-keyring.enable = true;
   };
 }
