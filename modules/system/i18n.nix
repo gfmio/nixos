0400e9c162e7426e5651d0235cf769252a8b0878
Frédérique Mittelstaedt <git@gfm.io>
2022-01-23 15:03:52 +0000

    update i18n config

--- modules/system/i18n.nix
+++ modules/system/i18n.nix
@@ -22,15 +22,27 @@ in
 
   config = mkIf cfg.enable {
     i18n = {
-      font = "Lat2-Terminus16";
-      # consoleFont = "latarcyrheb-sun32"; # Big console font for HiDPI
-      # consoleKeyMap = "fr";
       defaultLocale = "en_US.UTF-8";
     };
-
-    # console = {
-    #   font = "Lat2-Terminus16";
-    #   keyMap = "us";
-    # };
+    console = {
+      font = "Lat2-Terminus16";
+      # font = "latarcyrheb-sun32"; # Big console font for HiDPI
+      keyMap = "us";
+      earlySetup = true;
+      # useXkbConfig = true;
+      # colors = [];
+      # example = [
+      #   "002b36" "dc322f" "859900" "b58900"
+      #   "268bd2" "d33682" "2aa198" "eee8d5"
+      #   "002b36" "cb4b16" "586e75" "657b83"
+      #   "839496" "6c71c4" "93a1a1" "fdf6e3"
+      # ];
+      # description = ''
+      #   The 16 colors palette used by the virtual consoles.
+      #   Leave empty to use the default colors.
+      #   Colors must be in hexadecimal format and listed in
+      #   order from color 0 to color 15.
+      # '';
+    };
   };
 }
