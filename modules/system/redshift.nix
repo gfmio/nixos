49f3a1e80f7fa3221b27ca2f28a79a959b9d58a1
Frédérique Mittelstaedt <git@gfm.io>
2022-01-24 00:23:58 +0000

    update redshift config

--- modules/system/redshift.nix
+++ modules/system/redshift.nix
@@ -29,10 +29,10 @@ in
         day = "1";
         night = "1";
       };
-      # temperature = {
-      #   day = 5500;
-      #   night = 3700;
-      # };
+      temperature = {
+        day = 5500;
+        night = 3700;
+      };
     };
     location = {
       provider = "manual";
