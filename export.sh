58416ed56abc511f210ff45a6c0f66771ab6fe8e
Frédérique Mittelstaedt <git@gfm.io>
2022-01-25 10:10:50 +0000

    update export script to support multiple hosts specified via an argument

--- export.sh
+++ export.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
 
+if [ ! -n "$1" ]; then
+  echo "No host was supplied";
+  exit 1;
+fi
+
+host=$1;
+
+if [[ ! -d hosts/$name ]]; then
+  echo "Unknown host $name";
+  exit 1;
+fi
+
 # System
 
-cat hosts/nixos-test-vm/system/configuration.nix | sudo tee /etc/nixos/configuration.nix
-cat hosts/nixos-test-vm/system/common.nix | sudo tee /etc/nixos/common.nix
-# cat hosts/nixos-test-vm/system/hardware-configuration.nix | sudo tee /etc/nixos/hardware-configuration.nix
-sudo rm -rf /etc/nixos/modules
-sudo cp -R modules/system /etc/nixos/modules
+cat hosts/$host/system/configuration.nix | sudo tee /etc/nixos/configuration.nix &&
+cat hosts/$host/system/common.nix | sudo tee /etc/nixos/common.nix &&
+# cat hosts/$host/system/hardware-configuration.nix | sudo tee /etc/nixos/hardware-configuration.nix
+sudo rm -rf /etc/nixos/modules &&
+sudo cp -R modules/system /etc/nixos/modules &&
 
 # User
 
-cat hosts/nixos-test-vm/user/home.nix | tee $HOME/.config/nixpkgs/home.nix
-cat hosts/nixos-test-vm/user/config.nix | tee $HOME/.config/nixpkgs/config.nix
-cat hosts/nixos-test-vm/user/common.nix | tee $HOME/.config/nixpkgs/common.nix
-rm -rf $HOME/.config/nixpkgs/modules
+cat hosts/$host/user/home.nix | tee $HOME/.config/nixpkgs/home.nix &&
+cat hosts/$host/user/config.nix | tee $HOME/.config/nixpkgs/config.nix &&
+cat hosts/$host/user/common.nix | tee $HOME/.config/nixpkgs/common.nix &&
+rm -rf $HOME/.config/nixpkgs/modules &&
 cp -R modules/user $HOME/.config/nixpkgs/modules
