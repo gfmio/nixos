45d2e808f4aeb075f526aa46b346fe05ec4d2d3c
Frédérique Mittelstaedt <git@gfm.io>
2023-01-16 22:08:58 +0000

    remove old

--- old/hosts/xiaomi/system/configuration.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, lib, ... }:
-
-{
-  imports = [
-    ./hardware-configuration.nix
-    ./common.nix
-    ./modules/modules.nix
-  ];
-}
--- old/import.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cat /etc/nixos/configuration.nix > nixos/system/configuration.nix
-cat /etc/nixos/hardware-configuration.nix > nixos/system/hardware-configuration.nix
-
-cat $HOME/.config/nixpkgs/home.nix > nixos/user/home.nix
-cat $HOME/.config/nixpkgs/config.nix > nixos/user/config.nix
