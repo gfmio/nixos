5aa89bcf739d514b78750e5d31f3f7582d048351
Frédérique Mittelstaedt <git@gfm.io>
2023-02-25 15:58:46 +0000

    add nix-ld to enable non-patched programs to run

--- nixosModules/system.nix
+++ nixosModules/system.nix
@@ -51,8 +51,12 @@ in {
 
     services.udisks2.enable = true;
 
+    # Enable USB auto-mount
     services.udev.extraRules = ''
       ACTION=="add", SUBSYSTEMS=="usb", SUBSYSTEM=="block", ENV{ID_FS_USAGE}=="filesystem", RUN{program}+="${pkgs.systemd}/bin/systemd-mount --no-block --automount=yes --collect $devnode /media"
     '';
+
+    # Enable non-patched programs to run
+    programs.nix-ld.enable = true;
   };
 }
