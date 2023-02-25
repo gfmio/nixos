877a18c62ca3ac2e56d531f9d3b908132581f90e
Frédérique Mittelstaedt <git@gfm.io>
2023-02-25 20:28:49 +0000

    add additional config to xdg nixos module

--- nixosModules/xdg.nix
+++ nixosModules/xdg.nix
@@ -24,18 +24,43 @@ in {
         };
         gtk = mkOption {
           type = types.bool;
-          default = false;
+          default = true;
         };
       };
     };
   };
 
   config = mkIf cfg.enable {
-    xdg.portal = {
-      enable = true;
-      wlr.enable = cfg.wlr;
-      # gtk portal needed to make gtk apps happy
-      extraPortals = mkIf cfg.gtk [ pkgs.xdg-desktop-portal-gtk ];
+    # xdg-desktop-portal works by exposing a series of D-Bus interfaces
+    # known as portals under a well-known name
+    # (org.freedesktop.portal.Desktop) and object path
+    # (/org/freedesktop/portal/desktop).
+    # The portal interfaces include APIs for file access, opening URIs,
+    # printing and others.
+    xdg = {
+      autostart = {
+        enable = true;
+      };
+      icons = {
+        enable = true;
+      };
+      menus = {
+        enable = true;
+      };
+      mime = {
+        enable = true;
+      };
+      sounds = {
+        enable = true;
+      };
+      portal = {
+        enable = true;
+        wlr = {
+          enable = cfg.wlr;
+        };
+        # gtk portal needed to make gtk apps happy
+        extraPortals = mkIf cfg.gtk [ pkgs.xdg-desktop-portal-gtk ];
+      };
     };
   };
 }
